45f60e98257500f605bca7226f211ba69d8fb5a2
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-02-09 15:14:56 +0000

    config changes

---
+++ downloadPlugins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pushd /opt/papermc/plugins
+
+curl -LO https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/build/libs/Geyser-Spigot.jar
+
+curl -LO https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/build/libs/floodgate-spigot.jar
+
+popd

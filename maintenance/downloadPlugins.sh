3be4f1cab6b4c9f8657793dc605feaf04386d202
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2026-04-11 23:25:57 +0000

    updated maintenance scripts

--- maintenance/downloadPlugins.sh
+++ maintenance/downloadPlugins.sh
@@ -9,7 +9,9 @@
 echo "####### Updating Geyser Plugins #######"
 
 #define geyser plugins
-geyserplugins=(geyser floodgate)
+# whole section can move to modrinth once this issue is closed:
+# https://github.com/GeyserMC/Floodgate/issues/609
+geyserplugins=(floodgate)
 for plugin in "${geyserplugins[@]}"
 do
   echo "####### Updating --$plugin-- #######"
@@ -22,21 +24,114 @@ done
 echo "####### Updating Modrinth Plugins ######"
 
 #define modrinth plugins
-modrinthplugins=(griefprevention fastasyncworldedit multiverse-core multiverse-inventories multiverse-signportals multiverse-portals multiverse-netherportals worldguard)
+modrinthplugins=(geyser voidgen viaversion luckperms griefprevention fastasyncworldedit multiverse-core multiverse-inventories multiverse-signportals multiverse-portals multiverse-netherportals worldguard)
 
 for plugin in "${modrinthplugins[@]}"
 do
   echo "####### Updating --$plugin-- #######"
   API_URL="https://api.modrinth.com/v2/project/${plugin}/version"
-  LATEST_JSON=$(curl -s "${API_URL}")
-  DOWNLOAD_URL=$(echo "$LATEST_JSON" | jq -r '.[0].files[0].url')
+  VERSIONS=$(curl -s "${API_URL}")
+  # Helper function to pick the first matching version for a loader
+  get_download_url_for_loader() {
+    local loader="$1"
+
+    # 1) Try RELEASE versions first
+    local release_url=$(echo "$VERSIONS" | jq -r --arg loader "$loader" '
+      [
+        .[]
+        | select(.version_type == "release")
+        | select(any(.game_versions[]; startswith("1.21")))
+        | select(any(.loaders[]; . == $loader))
+      ]
+      | sort_by(.date_published)
+      | reverse
+      | .[0].files[0].url
+    ')
+
+    if [[ -n "$release_url" && "$release_url" != "null" ]]; then
+      echo "$release_url"
+      return
+    fi
+
+    # 2) Fall back to ANY version (beta, alpha, rc…)
+    echo "$VERSIONS" | jq -r --arg loader "$loader" '
+      [
+        .[]
+        | select(any(.game_versions[]; startswith("1.21")))
+        | select(any(.loaders[]; . == $loader))
+      ]
+      | sort_by(.date_published)
+      | reverse
+      | .[0].files[0].url
+    '
+  }
+
+  # Priority: paper → spigot → bukkit
+  DOWNLOAD_URL=$(get_download_url_for_loader "paper")
+
   if [[ -z "$DOWNLOAD_URL" || "$DOWNLOAD_URL" == "null" ]]; then
-  echo "Failed to get download URL"
-  exit 1
+    DOWNLOAD_URL=$(get_download_url_for_loader "spigot")
   fi
+
+  if [[ -z "$DOWNLOAD_URL" || "$DOWNLOAD_URL" == "null" ]]; then
+    DOWNLOAD_URL=$(get_download_url_for_loader "bukkit")
+  fi
+
+  if [[ -z "$DOWNLOAD_URL" || "$DOWNLOAD_URL" == "null" ]]; then
+    echo "Failed to find a compatible version for $plugin"
+    exit 1
+  fi
+
   echo "########## curl -L $DOWNLOAD_URL -o /opt/papermc/plugins/${plugin}.jar"
   curl -fsL "$DOWNLOAD_URL" -o "/opt/papermc/plugins/${plugin}.jar"
   echo "####### Downloading --$plugin-- finished with exit code --$?-- #######"
+done
+
+
+echo "####### Updating Hangar Plugins ######"
+
+hangarplugins=(
+  "ScreamingSandals/ScreamingBedWars"
+  "MrMicky/WorldEditSelectionVisualizer"
+)
+
+for plugin in "${hangarplugins[@]}"; do
+  AUTHOR="${plugin%%/*}"
+  PROJECT="${plugin##*/}"
+
+  echo "####### Updating --$PROJECT-- #######"
+
+  API_URL="https://hangar.papermc.io/api/v1/projects/${AUTHOR}/${PROJECT}/versions"
+  VERSIONS_JSON=$(curl -s -H "User-Agent: joerg-mc-updater/1.0" "$API_URL")
+
+  # Find latest version supporting MC 1.21
+  VERSION=$(echo "$VERSIONS_JSON" | jq -r '
+    .result[]
+    | select(.platformDependencies.PAPER | index("1.21"))
+    | .name
+  ' | sort -V | tail -n 1)
+
+  if [[ -z "$VERSION" || "$VERSION" == "null" ]]; then
+    echo "Failed to find compatible version for $PROJECT"
+    continue
+  fi
+
+  echo "Latest compatible version: $VERSION"
+
+  VERSION_API="https://hangar.papermc.io/api/v1/projects/${AUTHOR}/${PROJECT}/versions/${VERSION}"
+  FILES_JSON=$(curl -s -H "User-Agent: mc-updater/1.0" "$VERSION_API")
+
+  DOWNLOAD_URL=$(echo "$FILES_JSON" | jq -r '.downloads.PAPER.downloadUrl')
+
+  if [[ -z "$DOWNLOAD_URL" || "$DOWNLOAD_URL" == "null" ]]; then
+    echo "Failed to get download URL for $PROJECT"
+    continue
+  fi
+
+  echo "########## curl -L $DOWNLOAD_URL -o /opt/papermc/plugins/${PROJECT}.jar"
+  curl -fsL -H "User-Agent: mc-updater/1.0" "$DOWNLOAD_URL" -o "/opt/papermc/plugins/${PROJECT}.jar"
+
+  echo "####### Downloading --$PROJECT-- finished with exit code --$?-- #######"
 done
 
 
@@ -44,8 +139,8 @@ echo "####### Updating Spiggot Plugins #######"
 
 # we get spiggot plugins via spiget api if we have the resourceid
 declare -A spiggotplugins
-spiggotplugins=(["28140"]=LuckPerms ["17311"]=WorldEditSelectionVisualizer ["44703"]=BuildBattle ["19254"]=ViaVersion ["25391"]=VoidGen ["63714"]=Bedwars)
-
+#spiggotplugins=(["17311"]=WorldEditSelectionVisualizer ["44703"]=BuildBattle ["63714"]=Bedwars)
+spiggotplugins=(["44703"]=BuildBattle)
 for plugin in "${!spiggotplugins[@]}"
 do
   echo "####### Updating -- ${spiggotplugins[${plugin}]} -- ResourceID: $plugin -- #######"
@@ -63,4 +158,16 @@ curl -fsL https://ci.citizensnpcs.co/job/citizens2/lastSuccessfulBuild/artifact/
 rm /opt/papermc/plugins/Citizen*.jar
 unzip -j /opt/papermc/plugins/citizen.zip -d /opt/papermc/plugins/
 rm /opt/papermc/plugins/citizen.zip
+
+#echo "####### Updating --denizen-- #######"
+#curl -fsL https://ci.citizensnpcs.co/job/Denizen/lastSuccessfulBuild/artifact/target/Denizen-*.jar/*zip*/target.zip -o /opt/papermc/plugins/denizen.zip
+#rm /opt/papermc/plugins/Denizen*.jar
+#unzip -j /opt/papermc/plugins/denizen.zip -d /opt/papermc/plugins/
+#rm /opt/papermc/plugins/denizen.zip
+
+#echo "####### Updating --depenizen-- #######"
+#curl -fsL https://ci.citizensnpcs.co/job/Depenizen/lastSuccessfulBuild/artifact/target/*zip*/target.zip -o /opt/papermc/plugins/depenizen.zip
+#rm /opt/papermc/plugins/Depenizen*.jar
+#unzip -j /opt/papermc/plugins/depenizen.zip -d /opt/papermc/plugins/
+#rm /opt/papermc/plugins/depenizen.zip
 
--- maintenance/update_server.sh
+++ maintenance/update_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "##### Update paper to latest version #####"
 #echo "skipping until newest is stable again"
-su minecraft -c 'python /opt/papermc/maintenance/server_update.py -o /opt/papermc/paper.jar'
+su minecraft -c 'python /opt/papermc/maintenance/server_update.py --version=1.21.11 -o /opt/papermc/paper.jar'
 echo "##### Update geyser/floodgate to latest version #####"
 su minecraft -c '/opt/papermc/maintenance/downloadPlugins.sh'
 echo "#####  Restarting Minecraft Server #####"
